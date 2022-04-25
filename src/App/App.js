02738c65995277db343c7745bb9463a77a1a290e
hussein <fathinsatrian358@gmail.com>
2022-04-26 00:26:19 +0700

    done validasi & konsumsi API method get

--- src/App/App.js
+++ src/App/App.js
@@ -2,16 +2,32 @@
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable consistent-return */
 /* eslint-disable space-before-function-paren */
-import React from 'react';
+import React, {useEffect, useState}from 'react'
 import './App.css';
 import AppBar from '../components/AppBar';
 import Container from '../components/Container';
+import axios from "axios"
 
-function App() {
+function App(props) {
+
+
+  const [quote, setQuote] = useState();
+  console.log(quote);
+  useEffect(()=>{
+    getQuote();
+},[])
+  const getQuote = () => {
+    axios.get('http://quotes.rest/qod?category=inspire')
+    .then(res=> {
+        console.log(res);
+        setQuote(res.data.contents.quotes[0].quote);
+    })
+}
+
   return (
     <div className='App'>
       <AppBar />
-      <Container/>
+      <Container text={`"${quote}"`}/>
     </div>
   );
 }
--- src/components/Container.js
+++ src/components/Container.js
@@ -1,17 +1,34 @@
-import React, {useState}from 'react'
+import React, { useState } from 'react'
 import './Container.css';
 import backgroundimg from '../assets/logo.png'
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button'
 
-
-function Container() {
+function Container(props) {
     const [email, setEmail] = useState();
     const [password, setPassword] = useState();
+    const [rawInput, setRawInput] = useState([])
     const [donasi, setDonasi] = useState();
+    const format = "@gmail.com" && "@googlemail.com"
+
+    // console.log(quote)
     console.log("email : ", email);
     console.log("password : ", password);
     console.log("donasi : ", donasi);
+
+    const handleInput = text => {
+        const val = text.target.value
+        const lastTypedChar = val[val.length - 1];
+        setRawInput(...rawInput, lastTypedChar)
+    }
+
+    const formatted = (numb) => {
+        const format = numb.toString().split('').reverse().join('');
+        const convert = format.match(/\d{1,3}/g);
+        const rupiah = 'Rp ' + convert.join('.').split('').reverse().join('')
+        setDonasi(rupiah)
+        return rupiah
+    }
     return (
         <div className='main-container index'>
             <div className='box box1 flx'>
@@ -19,42 +36,74 @@ function Container() {
                     <h2 style={{ color: 'black', fontWeight: '400', marginLeft: '8%', marginTop: '5%' }}>Mari Donasi</h2>
                 </div>
                 <div className='input'>
-                    <TextField
-                        id="outlined-basic"
-                        label="Email"
-                        variant="outlined"
-                        onChange={text => setEmail(text.target.value)}
-                        // id="outlined-basic" label="Outlined" variant="outlined"
-                        // error
-                        // id="outlined-error-helper-text"
-                        // label="Error"
-                        // defaultValue="Hello World"
-                        // helperText="Incorrect entry."
-                        // onChange={}
-                        style={{ width: '75%', margin: 13 }}
-                    />
-                    <TextField
+                    {
+                        email?.includes(format) ? <TextField
+                            id="outlined-basic"
+                            label="Email"
+                            variant="outlined"
+                            onChange={text => setEmail(text.target.value)}
+                            // id="outlined-basic" label="Outlined" variant="outlined"
+                            // error
+                            // id="outlined-error-helper-text"
+                            // label="Error"
+                            // defaultValue="Hello World"
+                            // helperText="Incorrect entry."
+                            // onChange={}
+                            style={{ width: '75%', margin: 13 }}
+                        /> : <TextField
+                            onChange={text => setEmail(text.target.value)}
+                            // id="outlined-basic" label="Outlined" variant="outlined"
+                            error
+                            id="outlined-error-helper-text"
+                            label="Email"
+                            // defaultValue="Hello World"
+                            helperText={!email ? "Email tidak boleh kosong!" : "Format email salah dan hanya menerima gmail"}
+                            // onChange={}
+                            style={{ width: '75%', margin: 13 }}
+                        />}
+                    {
+                        !password
+                            ?
+                            <TextField
 
-                        id="outlined-password-input"
-                        label="Password"
-                        type="password"
-                        autoComplete="current-password"
-                        onChange={text => setPassword(text.target.value)}
-                        
-                        // error
-                        // id="outlined-error-helper-text"
-                        // label="Error"
-                        // defaultValue="Hello World"
-                        // helperText="Incorrect entry."
-                        // onChange={}
-                        style={{ width: '75%', margin: 13 }}
-                    />
+                                id="outlined-password-input"
+                                label="Password"
+                                type="password"
+                                autoComplete="current-password"
+                                onChange={text => setPassword(text.target.value)}
+                                error
+                                // id="outlined-error-helper-text"
+                                // label="Error"
+                                // defaultValue="Hello World"
+                                helperText={!password ? "Password tidak boleh kosong!" : null}
+                                // onChange={}
+                                style={{ width: '75%', margin: 13 }}
+                            />
+                            :
+                            <TextField
+
+                                id="outlined-password-input"
+                                label="Password"
+                                type="password"
+                                autoComplete="current-password"
+                                onChange={text => setPassword(text.target.value)}
+                                // error
+                                // id="outlined-error-helper-text"
+                                // label="Error"
+                                // defaultValue="Hello World"
+                                // helperText={!password ? "Password tidak boleh kosong!" : null}
+                                // onChange={}
+                                style={{ width: '75%', margin: 13 }}
+                            />
+                    }
+
                     <TextField
                         id="outlined-basic"
                         label="Donasi"
                         variant="outlined"
-                        type='number'
-                        onChange={text => setDonasi(text.target.value)}
+                        type='text'
+                        onChange={handleInput}
+                        value={formatted}
                         // error
                         // id="outlined-error-helper-text"
                         // label="Error"
@@ -68,7 +117,7 @@ function Container() {
                 <div className='quotes'>
                     <div className='qts'>
                         <h4>Quotes of the day:</h4>
-                        <p className='qtsText'>"lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum"</p>
+                        <p className='qtsText'>{props.text}</p>
                     </div>
 
                 </div>
