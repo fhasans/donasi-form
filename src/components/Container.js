8400e3dfce86aa2064d82fa04fe5543e7d59458f
hussein <fathinsatrian358@gmail.com>
2022-04-26 10:58:40 +0700

    project donasi form finished

--- src/components/Container.js
+++ src/components/Container.js
@@ -7,28 +7,30 @@ import Button from '@mui/material/Button'
 function Container(props) {
     const [email, setEmail] = useState();
     const [password, setPassword] = useState();
-    const [rawInput, setRawInput] = useState([])
     const [donasi, setDonasi] = useState();
     const format = "@gmail.com" && "@googlemail.com"
+    const [formatted, setFormattedInput] = useState("");
+    
+    const handleInput = e => {
+        const val = e.target.value;        
+        const rupiah = (number)=>{
+            return `Rp. ${number.replace(/,/g,"").replace(/[^0-9]/g,"").replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`;
+          }
+        //   console.log(getDec);
+
+        //   const normalize = getDec[0] && getDec[0].replace(/[^0-9]/g, "");
+        const formatted = rupiah(val).toString();
+        setFormattedInput(formatted);
+        // console.log("normalize: ", normalize);
+        // console.log("formatted:  ", formatted);
+    }
 
     // console.log(quote)
     console.log("email : ", email);
     console.log("password : ", password);
     console.log("donasi : ", donasi);
 
-    const handleInput = text => {
-        const val = text.target.value
-        const lastTypedChar = val[val.length - 1];
-        setRawInput(...rawInput, lastTypedChar)
-    }
-
-    const formatted = (numb) => {
-        const format = numb.toString().split('').reverse().join('');
-        const convert = format.match(/\d{1,3}/g);
-        const rupiah = 'Rp ' + convert.join('.').split('').reverse().join('')
-        setDonasi(rupiah)
-        return rupiah
-    }
+    
     return (
         <div className='main-container index'>
             <div className='box box1 flx'>
@@ -36,34 +38,28 @@ function Container(props) {
                     <h2 style={{ color: 'black', fontWeight: '400', marginLeft: '8%', marginTop: '5%' }}>Mari Donasi</h2>
                 </div>
                 <div className='input'>
-                    {
-                        email?.includes(format) ? <TextField
-                            id="outlined-basic"
-                            label="Email"
-                            variant="outlined"
+                    <TextField
                             onChange={text => setEmail(text.target.value)}
                             // id="outlined-basic" label="Outlined" variant="outlined"
-                            // error
-                            // id="outlined-error-helper-text"
-                            // label="Error"
-                            // defaultValue="Hello World"
-                            // helperText="Incorrect entry."
-                            // onChange={}
-                            style={{ width: '75%', margin: 13 }}
-                        /> : <TextField
-                            onChange={text => setEmail(text.target.value)}
-                            // id="outlined-basic" label="Outlined" variant="outlined"
-                            error
+                            error={
+                                !email
+                                ?
+                                true
+                                :
+                                email.match(/^[a-z0-9](\.?[a-z0-9]){5,}@g(oogle)?mail\.com$/)
+                                ?
+                                false
+                                :
+                                true
+                                
+                            }
                             id="outlined-error-helper-text"
                             label="Email"
                             // defaultValue="Hello World"
-                            helperText={!email ? "Email tidak boleh kosong!" : "Format email salah dan hanya menerima gmail"}
+                            helperText={!email ? "Email tidak boleh kosong!" : email.match(/^[a-z0-9](\.?[a-z0-9]){5,}@g(oogle)?mail\.com$/) ? null : "Format email salah dan hanya menerima gmail" }
                             // onChange={}
                             style={{ width: '75%', margin: 13 }}
-                        />}
-                    {
-                        !password
-                            ?
+                        />
                             <TextField
 
                                 id="outlined-password-input"
@@ -71,7 +67,7 @@ function Container(props) {
                                 type="password"
                                 autoComplete="current-password"
                                 onChange={text => setPassword(text.target.value)}
-                                error
+                                error={!password ? true : false}
                                 // id="outlined-error-helper-text"
                                 // label="Error"
                                 // defaultValue="Hello World"
@@ -79,37 +75,16 @@ function Container(props) {
                                 // onChange={}
                                 style={{ width: '75%', margin: 13 }}
                             />
-                            :
-                            <TextField
-
-                                id="outlined-password-input"
-                                label="Password"
-                                type="password"
-                                autoComplete="current-password"
-                                onChange={text => setPassword(text.target.value)}
-                                // error
-                                // id="outlined-error-helper-text"
-                                // label="Error"
-                                // defaultValue="Hello World"
-                                // helperText={!password ? "Password tidak boleh kosong!" : null}
-                                // onChange={}
-                                style={{ width: '75%', margin: 13 }}
-                            />
-                    }
 
                     <TextField
                         id="outlined-basic"
                         label="Donasi"
                         variant="outlined"
                         type='text'
+                        error={!donasi ? true : false}
                         onChange={handleInput}
                         value={formatted}
-                        // error
-                        // id="outlined-error-helper-text"
-                        // label="Error"
-                        // defaultValue="Hello World"
-                        // helperText="Incorrect entry."
-                        // onChange={}
+                        helperText={!donasi ? "Input Donasi tidak boleh kosong!" : null}
                         style={{ width: '75%', margin: 13 }}
                     />
                     <Button variant="contained" style={{ width: '75%', height: '16%', margin: 13 }}>Submit</Button>
